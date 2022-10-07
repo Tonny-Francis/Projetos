ed8178582853ad705afa1adfb9443f876581bdf2
Tonny Francis <tonnyfrancis161@poli.ufrj.br>
2022-10-07 11:30:58 -0300

    Event

--- Convem-PS/src/app/components/box/box.component.ts
+++ Convem-PS/src/app/components/box/box.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-box',
   templateUrl: './box.component.html',
   styleUrls: ['./box.component.css']
 })
+
 export class BoxComponent implements OnInit {
-  empresa:string = 'Convem';
-  constructor() { }
+  //Variáveis para interpolação
+  empresa: string = 'Convem';
+  value: String = '';
+
+  //Funções
+  button_click(){
+    if(this.value.toLowerCase() === 'sim'){
+      //Devemos chamar a API Rest e consultar a base de dados contendo a palavra sucesso
+      console.log('Sucesso')
+    }
+    else{
+      //Mensagem de erro
+      alert('Erro')
+    }
+  }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
