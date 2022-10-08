9f75429f9b204cf67ca8c365b9784e0723629605
Tonny Francis <tonnyfrancis161@poli.ufrj.br>
2022-10-08 11:20:21 -0300

    filtrando sucesso

--- Convem-PS/src/app/components/box/box.component.ts
+++ Convem-PS/src/app/components/box/box.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ApiService } from 'src/app/services/api.service';
 import { Data } from 'src/app/interfaces/Data';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-box',
@@ -11,9 +12,12 @@ import { Data } from 'src/app/interfaces/Data';
 
 export class BoxComponent implements OnInit {
   //Variáveis para interpolação
-  empresa: string = 'Convem';
-  value: string = '';
-  data: Data[] = []
+  empresa: String = 'Convem';
+  busca: any = 'sucesso';
+  value: String = '';
+  interface: Data[] = [];
+  bool: boolean = false;
+  tamanho: Number = 0;
 
   constructor(private apiservice: ApiService) {}
 
@@ -24,14 +28,32 @@ export class BoxComponent implements OnInit {
     if(this.value.toLowerCase() == 'sim'){
       //Devemos chamar a API Rest e consultar a base de dados contendo a palavra sucesso
       this.getData()
-      console.log(this.data.values())
+      setTimeout(() => {
+        this.filter()
+        console.log(this.bool)
+      }, 1000);
     }
     else{
       //Mensagem de erro
+      this.bool = false;
       alert('Erro')
     }
   }
   getData(): void{
-    this.apiservice.getAll().subscribe((data) => this.data = data)
+    this.apiservice.getAll().subscribe((data) => this.interface = data)
+  }
+  filter(){
+    this.interface = this.interface.filter(
+    function(obj)
+    {
+      return obj.data == 'data 2'
+    })
+    for(let dado of this.interface)
+    {
+      if(dado.data == 'data 2')
+        this.bool = true;
+      else
+        this.bool = false;
+    }
   }
 }
