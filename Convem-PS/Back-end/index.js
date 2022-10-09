4e4110b7b8f7b57f06cebaebbc77ea219a4af6d5
Tonny Francis <tonnyfrancis161@poli.ufrj.br>
2022-10-09 13:29:33 -0300

    heroku

--- Convem-PS/Back-end/index.js
+++ Convem-PS/Back-end/index.js
@@ -3,12 +3,6 @@ const express = require('express')
 const server = express()
 const port = process.env.PORT
 
-server.use((req, res) =>{
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET")
-    res.header("Access-Control-Allow-Methods", "GET, OPTIONS")
-    res.header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding")
-})
 //Rotas
 server.get('/sim', async (req, res) =>{
     res.status(200).json({answer: 'sucesso'})
