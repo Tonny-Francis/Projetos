d3ad1dde64581a4320510d675207ac28e84200d4
Tonny Francis <tonnyfrancis161@poli.ufrj.br>
2022-10-12 22:12:49 -0300

    make it better

--- Convem-PS/Back-end/index.js
+++ Convem-PS/Back-end/index.js
@@ -11,7 +11,8 @@ server.use((req, res, next) =>{
     res.header('Access-Control-Allow-Credentials', 'true')
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-
+    res.header('Access-Control-Max-Age', '86400')
+    
     if(req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
     }
